2a59709451e952115c07bd58b2f960721ae9c5b4
Joost Cassee <joost@cassee.net>
2016-04-13 22:13:53 +0200

    Update Ansible setup

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -2,7 +2,9 @@
 
 set -e
 
-pip install ansible
-
-cd ansible
-ansible-playbook deploy.yml
+docker run \
+    --volume "$PWD"/ansible:/ansible:ro \
+    --env ANSIBLE_VAULT_PASSWORD="$ANSIBLE_VAULT_PASSWORD" \
+    --tty \
+    jcassee/ansible \
+    ssh-agent ansible-playbook -e add_ssh_key=yes deploy.yml
