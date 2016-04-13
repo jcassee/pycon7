814519b3a494e2b134e09bf43af451ff1a3a9ff1
Joost Cassee <joost@cassee.net>
2016-04-14 00:53:39 +0200

    Rename initial_config.yml to .yaml

--- scripts/server.sh
+++ scripts/server.sh
@@ -3,6 +3,6 @@
 set -e
 
 python manage.py migrate --noinput
-python manage.py loaddata initial_data.yml
+python manage.py loaddata initial_data.yaml
 
 uwsgi --master --http-socket=0.0.0.0:80 --module=registronavale.wsgi:application
