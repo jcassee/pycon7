2d5e441bc5a33a75591027cc4e0be425bd4221e9
Joost Cassee <joost@cassee.net>
2016-04-13 22:58:31 +0200

    Test in Docker container

--- features/environment.py
+++ features/environment.py
@@ -1,4 +1,5 @@
 def before_scenario(context, scenario):
+    # Use real URLs in output
     context.request_kwargs = {
         'SERVER_NAME': 'api.registronavale.com',
         'SERVER_PORT': str('443'),
--- scripts/test.sh
+++ scripts/test.sh
@@ -2,5 +2,9 @@
 
 set -e
 
-pip install -r requirements.txt
-python manage.py behave
+docker run \
+    --volume "$PWD":/src:ro \
+    --workdir /src \
+    --tty \
+    python:3.5 \
+    sh -c "pip install -r requirements.txt && python manage.py behave"
