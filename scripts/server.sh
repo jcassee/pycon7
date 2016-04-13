62e59675a4768baccec84e8e3950fc7a609f55b4
Joost Cassee <joost@cassee.net>
2016-04-14 00:43:06 +0200

    Fix loaddata command

--- scripts/server.sh
+++ scripts/server.sh
@@ -3,6 +3,6 @@
 set -e
 
 python manage.py migrate --noinput
-python manage.py loaddata initial_data.yml --noinput
+python manage.py loaddata initial_data.yml
 
 uwsgi --master --http-socket=0.0.0.0:80 --module=registronavale.wsgi:application
