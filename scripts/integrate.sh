5985f02296111eb481a0c20f69916fdff235edd4
Joost Cassee <joost@cassee.net>
2016-04-14 12:44:16 +0200

    Show nice steps in integration log

--- scripts/integrate.sh
+++ scripts/integrate.sh
@@ -5,8 +5,11 @@ set -e
 dirname=$(dirname "$0")
 cd "$dirname/.."
 
+i=0
 section() {
   [ -z "$TRAVIS" ] || echo -en "travis_fold:start:$1\\r"
+  i=$((i+1))
+  echo "Step $i: $1..."
   scripts/integrate/"$1".sh
   [ -z "$TRAVIS" ] || echo -en "travis_fold:end:$1\\r"
 }
